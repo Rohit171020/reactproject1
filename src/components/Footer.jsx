f7bc21c1e283d2e782ceb8674473d1d0ec4186ce
Rohit Gupta <rohitgupta63564@gmail.com>
2023-06-17 23:23:32 +0530

    c

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/rohit_gupta_0101?igshid=MzNlNGNkZWQ4Mg==" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Rohit171020" target={"blank"}>
+            Github
+          </a>
+          <a href="https://www.linkedin.com/in/rohit-gupta-16a964200/" target={"blank"}>
+          Linkedin
+          </a>
+      </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
